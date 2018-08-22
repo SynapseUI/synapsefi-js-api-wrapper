59aebd84f7e753c535eeef540108470550d4ae4f
Sean Hong <iamseanhong@gmail.com>
2018-08-22 14:04:50 -0700

    Pass test for get doc types, entity types, and entity scopes

--- test/fetchData.test.js
+++ test/fetchData.test.js
@@ -2,24 +2,74 @@ const { expect } = require('chai');
 
 const apiReqsWithLessArgs = require('./apiReqsWithLessArgs');
 
-it('fetch doc types', async () => {
-  const {
-    data: { physical_docs, social_docs, virtual_docs },
-  } = await apiReqsWithLessArgs.GET_USERS_DOCUMENT_TYPES();
+describe('Fetch data form /users endpoint', () => {
+  it('fetch doc types (physical, social, virtual)', async () => {
+    const {
+      data: { physical_docs, social_docs, virtual_docs },
+    } = await apiReqsWithLessArgs.GET_USERS_DOCUMENT_TYPES();
 
-  expect(physical_docs.map(({ type }) => type)).to.include.members([
-    'GOVT_ID',
-    'SSN_CARD',
-    'EIN_DOC',
-  ]);
+    expect(physical_docs.map(({ type }) => type)).to.include.members([
+      'GOVT_ID',
+      'SSN_CARD',
+      'EIN_DOC',
+    ]);
 
-  social_docs.map(({ type }) => {
-    console.log('s type: ', type);
-    return type;
+    expect(social_docs.map(({ type }) => type)).to.include.members([
+      'FACEBOOK',
+      'LINKEDIN',
+      'EMAIL_2FA',
+    ]);
+
+    expect(virtual_docs.map(({ type }) => type)).to.include.members([
+      'SSN',
+      'PASSPORT',
+      'DRIVERS_LICENSE',
+    ]);
   });
 
-  virtual_docs.map(({ type }) => {
-    console.log('v type: ', type);
-    return type;
+  it('fetches doc types (physical, social, virtual)', async () => {
+    const {
+      data: { physical_docs, social_docs, virtual_docs },
+    } = await apiReqsWithLessArgs.GET_USERS_DOCUMENT_TYPES();
+
+    expect(physical_docs.map(({ type }) => type)).to.include.members([
+      'GOVT_ID',
+      'SSN_CARD',
+      'EIN_DOC',
+    ]);
+
+    expect(social_docs.map(({ type }) => type)).to.include.members([
+      'FACEBOOK',
+      'LINKEDIN',
+      'EMAIL_2FA',
+    ]);
+
+    expect(virtual_docs.map(({ type }) => type)).to.include.members([
+      'SSN',
+      'PASSPORT',
+      'DRIVERS_LICENSE',
+    ]);
+  });
+
+  it('fetches entity types', async () => {
+    const { data: { BUSINESS, PERSONAL } } = await apiReqsWithLessArgs.GET_USERS_ENTITY_TYPES();
+
+    expect(BUSINESS).to.deep.include.members([
+      { common_name: 'Limited Liability Company', type: 'LLC' },
+      { common_name: 'Business Trust', type: 'TRUST' },
+      { common_name: 'Individual Retirement Accounts', type: 'IRA' },
+    ]);
+
+    expect(PERSONAL).to.deep.include.members([
+      { common_name: 'Male', type: 'M' },
+      { common_name: 'Female', type: 'F' },
+      { common_name: 'Personal Trust Account', type: 'TRUST' },
+    ]);
+  });
+
+  it('fetches entity scopes', async () => {
+    const { data: { scopes } } = await apiReqsWithLessArgs.GET_USERS_ENTITY_SCOPES();
+
+    expect(scopes).to.include.members(['Not Known', 'Airport', 'Arts & Entertainment']);
   });
 });
