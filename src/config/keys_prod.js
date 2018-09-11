50f22b1cda124e08428de647e1ac4edb7e0a1607
Sean Hong <iamseanhong@gmail.com>
2018-09-10 20:01:13 -0700

    Hide keys

---
+++ src/config/keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  host: 'https://api.synapsefi.com',
+  client_id: 'dummy_client_id',
+  client_secret: 'dummy_client_secret',
+  oauth_key: 'dummy_oauth_key',
+  fingerprint: 'dummy_fingerprint',
+  user_id: 'dummy_user_id',
+  refresh_token: 'dummy_refresh_token',
+  ip_address: 'dummy_ip_address',
+};
--- test/testHelper/platformUserApiCannon.js
+++ test/testHelper/platformUserApiCannon.js
@@ -1,14 +1,25 @@
+const {
+  host,
+  client_id,
+  client_secret,
+  oauth_key,
+  fingerprint,
+  user_id,
+  refresh_token,
+  ip_address,
+} = require('../../src/config/keys');
+
 const ApiFactory = require('../../src/ApiFactoryRelated/ApiFactory');
 
 const platformUserApiCannon = new ApiFactory({
-  host: 'https://uat-api.synapsefi.com',
-  client_id: 'client_id_HxEShtR1GVi9QaIoM6JTZnBUu8sq0CeXKplW3FPL',
-  client_secret: 'client_secret_qVtj0WIeaSYdhuwsZTz53P0EADQXxKN71v6H4mbf',
-  oauth_key: 'oauth_7cSblJWGhm10DF0BXisg3wdMExaetAyzK9Q8j5pL',
-  fingerprint: 'cc5127b4f73f02f78bf7ff785819f899',
-  user_id: '5b92cc307ce6f953dccbcc79',
-  refresh_token: 'refresh_8UgLhzOFJdx24o0V9XGl06uKD1pPRjQkE5rbmCHM',
-  ip_address: '123.0.0.1',
+  host,
+  client_id,
+  client_secret,
+  oauth_key,
+  fingerprint,
+  user_id,
+  refresh_token,
+  ip_address,
 });
 
 module.exports = platformUserApiCannon;
