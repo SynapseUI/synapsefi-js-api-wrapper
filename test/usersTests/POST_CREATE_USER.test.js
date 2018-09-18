0b2b420968b5c229f2ea1685aee4b57a2fece84a
Sean Hong <iamseanhong@gmail.com>
2018-09-18 16:40:09 -0700

    Test optional body params at post user

--- test/usersTests/POST_CREATE_USER.test.js
+++ test/usersTests/POST_CREATE_USER.test.js
@@ -7,32 +7,69 @@ const ApiFactory = require('../../src/ApiFactoryRelated/ApiFactory');
 //   - create user legal_name with "Post User"
 //   - `expect get user to have response with legal name "Post User"`
 //   - delete user
-it('POST_CREATE_USER', async () => {
-  // -----------------------------------------------------------------------------------
-  const {
-    data: { _id, refresh_token, legal_names },
-  } = await platformUserApiCannon.POST_CREATE_USER({
-    logins: [{ email: 'email.com' }],
-    phone_numbers: ['123.123.1233'],
-    legal_names: ['Post User'],
+describe('POST_CREATE_USER', () => {
+  it('create user with base info', async () => {
+    // -----------------------------------------------------------------------------------
+    const {
+      data: { _id, refresh_token, legal_names },
+    } = await platformUserApiCannon.POST_CREATE_USER({
+      logins: [{ email: 'email.com' }],
+      phone_numbers: ['123.123.1233'],
+      legal_names: ['Post User'],
+    });
+    // -----------------------------------------------------------------------------------
+
+    expect(legal_names[0]).to.equal('Post User');
+
+    const endUserApiCannon = new ApiFactory({
+      host: platformUserApiCannon.host,
+      client_id: platformUserApiCannon.client_id,
+      client_secret: platformUserApiCannon.client_secret,
+      fingerprint: platformUserApiCannon.fingerprint,
+      ip_address: platformUserApiCannon.ip_address,
+      refresh_token,
+      user_id: _id,
+      oauth_key: '',
+    });
+
+    const { data: { oauth_key } } = await endUserApiCannon.POST_OAUTH_USER();
+    endUserApiCannon.oauth_key = oauth_key;
+
+    await endUserApiCannon.PATCH_USER_PERMISSION({ permissionStr: 'MAKE-IT-GO-AWAY' });
   });
-  // -----------------------------------------------------------------------------------
 
-  expect(legal_names[0]).to.equal('Post User');
+  it.only('optionalBodyParams', async () => {
+    // -----------------------------------------------------------------------------------
+    const {
+      data: { _id, refresh_token, legal_names, extra },
+    } = await platformUserApiCannon.POST_CREATE_USER({
+      logins: [{ email: 'email.com' }],
+      phone_numbers: ['123.123.1233'],
+      legal_names: ['Post User'],
+      optionalBodyParams: {
+        extra: {
+          public_note: 'yeah i am a public note',
+        },
+      },
+    });
+    // -----------------------------------------------------------------------------------
 
-  const endUserApiCannon = new ApiFactory({
-    host: platformUserApiCannon.host,
-    client_id: platformUserApiCannon.client_id,
-    client_secret: platformUserApiCannon.client_secret,
-    fingerprint: platformUserApiCannon.fingerprint,
-    ip_address: platformUserApiCannon.ip_address,
-    refresh_token,
-    user_id: _id,
-    oauth_key: '',
+    expect(extra.public_note).to.equal('yeah i am a public note');
+
+    const endUserApiCannon = new ApiFactory({
+      host: platformUserApiCannon.host,
+      client_id: platformUserApiCannon.client_id,
+      client_secret: platformUserApiCannon.client_secret,
+      fingerprint: platformUserApiCannon.fingerprint,
+      ip_address: platformUserApiCannon.ip_address,
+      refresh_token,
+      user_id: _id,
+      oauth_key: '',
+    });
+
+    const { data: { oauth_key } } = await endUserApiCannon.POST_OAUTH_USER();
+    endUserApiCannon.oauth_key = oauth_key;
+
+    await endUserApiCannon.PATCH_USER_PERMISSION({ permissionStr: 'MAKE-IT-GO-AWAY' });
   });
-
-  const { data: { oauth_key } } = await endUserApiCannon.POST_OAUTH_USER();
-  endUserApiCannon.oauth_key = oauth_key;
-
-  await endUserApiCannon.PATCH_USER_PERMISSION({ permissionStr: 'MAKE-IT-GO-AWAY' });
 });
