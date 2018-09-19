d4c30e6ecba154600d8d496cd9625535b4c0299f
Sean Hong <iamseanhong@gmail.com>
2018-09-19 14:11:30 -0700

    Update globalTestHelper

--- test/globalTestHelper.test.js
+++ test/globalTestHelper.test.js
@@ -2,14 +2,22 @@ const platformUserApiCannon = require('./testHelper/platformUserApiCannon');
 const testHelpersForNodes = require('./testHelper/testHelpersForNodes');
 
 before(async () => {
-  console.log('Set refresh_token');
-  await platformUserApiCannon.GET_USER();
+  try {
+    console.log('Set refresh_token');
+    await platformUserApiCannon.GET_USER();
 
-  console.log('Set oauth_key');
-  await platformUserApiCannon.POST_OAUTH_USER();
+    console.log('Set oauth_key');
+    await platformUserApiCannon.POST_OAUTH_USER();
+  } catch (error) {
+    console.log('error: ', error.response.data.error.en);
+  }
 });
 
 after(async () => {
-  console.log('If there are any node left then delete them.');
-  await testHelpersForNodes.deleteAllNodeFromPlatformUser();
+  try {
+    console.log('If there are any node left then delete them.');
+    await testHelpersForNodes.deleteAllNodeFromPlatformUser();
+  } catch (error) {
+    console.log('error: ', error.response.data.error.en);
+  }
 });
